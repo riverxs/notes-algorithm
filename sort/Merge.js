a9f8751ca7762fc8c3acd4d87ed3908d20cbf62a
riverxs <sxj623@gmail.com>
2016-05-04 22:44:25 +0800

    归并排序

--- sort/Merge.js
+++ sort/Merge.js
@@ -9,31 +9,38 @@ class Merge {
 	}
 
 	sort(a) {
-		msort(a, 0, a.length -1)
+		this.msort(a, 0, a.length -1)
+		return a
 	}
 
 	msort(a, low, high) {
 		if(high <= low) return
 		let mid = low + parseInt((high - low)/2)
-		msort(a, low, mid)
-		msort(a, mid+1, high)
-		merge(a, low, mid, high)
+		// 左半边有序
+		this.msort(a, low, mid)
+		// 右半边有序
+		this.msort(a, mid+1, high)
+		// 将排好序的部分归并
+		this.merge(a, low, mid, high)
 	}
 	// 归并方法：原地归并
 	merge(a, low, mid, high) {
 		// 将a[low...mid]和a[mid+1...high]归并
-		let i = lo, j = mid + 1
-		// 将a复制到aux
+		let i = low, j = mid + 1
+		// 将a复制到辅助数组aux
 		for(let k = low; k <= high; k++){	
-			this.aux[K] = a[k]
+			this.aux[k] = a[k]
 		}
 		for(let k = low; k <= high; k++){
+			// 左半边已用完，将右半边的元素aux[j]作为排序的数组的元素a[k]
 			if(i > mid){
 				a[k] = this.aux[j++]
 			}
+			// 右半边已用完，将左半边的元素aux[i]作为排序的数组的元素a[k]
 			else if(j > high){
 				a[k] = this.aux[i++]
 			}
+			// 如果右半边的元素小于左半边的元素，那么将右半边元素aux[j]作为a[k]
 			else if(this.less(this.aux[j], this.aux[i])){
 				a[k] = this.aux[j++]
 			}
