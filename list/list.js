0a391647e182d55cb4946f222553624fb4ad73b3
riverxs <sxj623@gmail.com>
2017-10-24 23:05:55 +0800

    add list

--- list/list.js
+++ list/list.js
@@ -13,7 +13,23 @@
 // 列表： 采用动态存储策略典型结构，在逻辑上构成一个线性序列
 
 
+// 循秩访问无法接受O(n)
 
+class List {
+  constructor(list) {
+    this.list = list
+  }
+
+  // 在节点p的n个前驱中，找到等于e的最后者
+  find(e, n, p) {
+    while(0 < n--) {
+      if(e == this.list[--p]) return p
+    }
+    return null
+  }
+
+  // 
+}
 
 
 
