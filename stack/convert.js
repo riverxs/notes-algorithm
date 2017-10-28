ff2f4b4428c5675f92b4d66b16401e918fcc7c9e
riverxs <sxj623@gmail.com>
2017-10-28 22:18:40 +0800

    add other convert

--- stack/convert.js
+++ stack/convert.js
@@ -7,19 +7,17 @@ const Stack = require('./index')
  * @class Convert
  */
 class Convert {
-	constructor() {
-		this._elements = new Stack()
-		this.res = []
-	}
 
-	convert(from, to) {
+	static convert(from, to) {
+		let _elements = new Stack()
+		let _res = []
 		let digit = ['0','1','2','3','4','5','6','7','8','9'] // 根据to设定
 		let codePointA = 'A'.codePointAt(0)
 
 		let diff = to - 10
 
 		if(diff > 23) {
-			throw new Error('beyond the alphabeta range')
+			throw new Error('beyond the alphabeta range!')
 		}
 		while(diff > 0) {
 			digit.push(String.fromCharCode(codePointA++))
@@ -27,15 +25,17 @@ class Convert {
 		}
 
 		while(from !== 0) {
-			this._elements.push(from%to) // 取余
+			let remainder = from%to
+
+			_elements.push(digit[remainder]) // 取余
 			from = Math.floor(from/to)  // 取整
 		} 
 
-		while(!this._elements.isEmpty()) {
-			this.res.push(this._elements.pop())
+		while(!_elements.isEmpty()) {
+			_res.push(_elements.pop())
 		}
 
-		return this.res.join('')
+		return _res.join('')
 	} 	
 }
 
--- stack/convert.test.js
+++ stack/convert.test.js
@@ -2,6 +2,12 @@
 const Convert = require('./convert')
 
 test('convert number to binary format', () => {
-  let convert = new Convert()
-  expect(convert.convert(255, 2)).toBe('11111111')
+  // let convert = new Convert()
+  expect(Convert.convert(255, 2)).toBe('11111111')
 })
+
+test('test hexadecimal convert', ()=>{
+  // let convert = new Convert()
+  expect(Convert.convert(255, 16)).toBe('FF')
+})
+
