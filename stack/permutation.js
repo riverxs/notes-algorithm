7311c4ecb2003845149dab6099f64f7f6a5ef29e
riverxs <sxj623@gmail.com>
2017-11-07 23:29:25 +0800

    add  evaluation

--- stack/permutation.js
+++ stack/permutation.js
@@ -1,4 +1,5 @@
 // stack application
+
 /* 
 栈混洗，有栈A，栈S，栈B
 只允许将A中的顶元素弹出并压入S，或将S的顶元素弹出并压入B中
@@ -6,5 +7,13 @@
 （stack permutation)
  */
 
+/* [p1, p2, p3,...pn] 是[1,2,3,...n]的栈混洗，当且仅当对于
+任意i < j不含模式[..., j+1, i, j]
+ */ 
 
- 
+// 判断序列为栈混洗
+
+function isPermutation(src, res) {
+  
+}
+
