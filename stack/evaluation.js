5abe03b1c0b98c9498b10174b4479e098edbeb1b
riverxs <sxj623@gmail.com>
2017-11-08 23:57:58 +0800

    M expression evalation

--- stack/evaluation.js
+++ stack/evaluation.js
@@ -1,3 +1,34 @@
 // 缓冲应用
 // 中缀表达式求值
+const Stack = require('./index')
 
+function evaluate(exp) {
+  let operateStack = new Stack()
+  let numberStack = new Stack()
+  // let atom = exp[0]
+  let i = 0
+
+  operateStack.push('\0')
+  while(!operateStack.isEmpty()) { // 逐个处理各字符，直至运算符栈空
+    let curr = exp[i]
+
+    if(isDigit(curr)) {
+      readDigit(curr, numberStack) //读入操作数
+    }else{
+      // 读入操作符
+      switch(orderBetween(operateStack.top(), curr)){
+        case '<':
+          operateStack.push(curr)
+          break
+        case '>':
+          
+      }
+    }
+  }
+
+}
+
+
+function orderBetween(top, curr) {
+
+}
