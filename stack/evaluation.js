2b7981bdd8a549cfe875bcb4e0a7a69e8ff84734
riverxs <sxj623@gmail.com>
2017-11-26 11:41:41 +0800

    expression evaluation done

--- stack/evaluation.js
+++ stack/evaluation.js
@@ -3,13 +3,21 @@
 const Stack = require('./index')
 
 
-// 定义优先级二维表
+// 定义优先级二维表，[栈顶][当前]运算符表
+// + - * / ^ ! ( ) \0
 const orderMatrix = [
-  [],
-  [],
-  [],
+  ['>', '>', '<', '<', '<', '<', '<', '>', '>'],
+  ['>', '>', '<', '<', '<', '<', '<', '>', '>'],
+  ['>', '>', '>', '>', '<', '<', '<', '>', '>'],
+  ['>', '>', '>', '>', '<', '<', '<', '>', '>'],
+  ['>', '>', '>', '>', '>', '<', '<', '>', '>'],
+  ['>', '>', '>', '>', '>', '<', ' ', '>', '>'],
+  ['<', '<', '<', '<', '<', '<', '<', '=', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  ['<', '<', '<', '<', '<', '<', '<', ' ', '=']  
 ]
 
+const opsList = ['+', '-', '*', '/', '^', '!', '(', ')', '\0']
 
 function evaluate(exp) {
   let operateStack = new Stack()
@@ -40,8 +48,8 @@ function evaluate(exp) {
             numberStack.push(caculate(op, numberStack.pop())) // 一元运算
           }else{
             // 二元运算
-            let number1 = numberStack.pop()
             let number2 = numberStack.pop()
+            let number1 = numberStack.pop()
             numberStack.push(caculate(op, number1, number2))
           }
           break
@@ -49,11 +57,19 @@ function evaluate(exp) {
     }
   }
 
+  return numberStack.pop()
+
 }
 
+function readDigit(cur, stack) {
+  stack.push(curr)
+}
 
 function orderBetween(top, curr) {
+  let topIndex = opsList.indexOf(top)
+  let currIndex = opsList.indexOf(curr)
 
+  return orderMatrix[topIndex][currIndex]
 }
 
 
@@ -62,7 +78,27 @@ function isDigit(digit) {
 }
 
 
-function caculate() {
-  
+function caculate(op, num1, num2) {
+  if(num2) {
+    // return num1 op num2
+    switch(op) {
+      case '+':
+        return num1 + num2 
+      case '-':
+        return num1 - num2
+      case '*':
+        return num1 * num2
+      case '/': 
+        return num1 / num2
+      case '^':
+        return num1**num2
+    }
+  }else{
+    return factorial(num1)
+  }
 }
 
+function factorial(num) {
+  if(num == 1) return 1
+  else return n * factorial(n-1)
+}
