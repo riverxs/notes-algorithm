fa7239f930e282f820182772eccf55e3ad1068f3
riverxs <sxj623@gmail.com>
2017-11-27 23:38:15 +0800

    add queue structrue impl with linklist

--- queue/index2.js
+++ queue/index2.js
@@ -1,20 +1,31 @@
 // 先入先出的数据结构 first in first out
-// 链表实现
+// 基于链表实现
 const List = require('../list/index')
 
 
 class Queue{
 	constructor(){
 		this.queue = new List()
+		this.tailEle = null
 	}
 	// 入队
 	enqueue(ele) {
 		this.queue.add(ele)
+		this.tailEle = ele
 	}
 	// 出队
 	dequeue() {
 		return this.queue.remove(this.queue.first.item)
 	}
+
+  tail() {
+    return this.tailEle
+  }
+
+  front() {
+    return this.queue.first.item
+  }
+
 }
 
 module.exports = Queue
