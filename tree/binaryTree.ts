cb12601ff77d9d2a3a67255b2e59938103f65f01
riverxs <sxj623@gmail.com>
2017-12-12 00:06:21 +0800

    add bst

--- tree/binaryTree.ts
+++ tree/binaryTree.ts
@@ -1,37 +1,77 @@
 // 二叉树抽象数据类型
 
-interface BinNode<T> {
-    parent: BinNode<T>;
-    left: BinNode<T>;
-    right: BinNode<T>;
+// interface BinNode<T> {
+//     parent: BinNode<T>;
+//     left: BinNode<T>;
+//     right: BinNode<T>;
 
-    data: T;
-    height: number;
+//     data: T;
+//     height: number;
     
-    size(): number; // 子树规模
+//     size(): number; // 子树规模
 
-    insertAsLeftChild(left: T): BinNode<T>;
-    insertAsRightChild(rigth: T): BinNode<T>;
+//     insertAsLeftChild(left: T): BinNode<T>;
+//     insertAsRightChild(rigth: T): BinNode<T>;
 
-    succ(): BinNode<T>;
+//     succ(): BinNode<T>;
 
-    traverseLevel<U>(child: U): void; // 子树层次遍历
-    traversePre<U>(child: U): void; // 子树先序遍历
-    traverseIn<U>(child: U): void; // 子树中序遍历
-    traversePost<U>(child: U): void; // 子树后序遍历
-}
+//     traverseLevel<U>(child: U): void; // 子树层次遍历
+//     traversePre<U>(child: U): void; // 子树先序遍历
+//     traverseIn<U>(child: U): void; // 子树中序遍历
+//     traversePost<U>(child: U): void; // 子树后序遍历
+// }
 
 
-class Node<T> implements BinNode<T> {
-    constructor(data: T, parent: BinNode<T>) {
+class Node<T> {
+    // parent: BinNode<T>;
+    // data: 
+    left: Node;
+    right: Node;
+    height: number;
+    constructor(public data: T, public parent: Node) {
         this.parent = parent;
-        this.data = data
-        this.left = null
-        this.right = null
+        this.data = data;
+        this.left = null;
+        this.right = null;
+        this.height = 0
     }
 
-    insertAsLeftChild(e: T) {
-
+    insertAsLeftChild(e: T): void {
+        this.left = new Node(e, this)
     }
+
+    insertAsRightChild(e: T){
+        this.right = new Node(e, this)
+    }
+
+    // 后代总数
+    size(): number {
+        var s: number = 1
+        if(this.left) s += this.left.size()
+        if(this.right) s += this.right.size()
+
+        return s
+    }
+
+    succ() {}
+
+    traverseLevel(){}
+    traversePre(){}
+    traverseIn(){}
+    traversePost(){}
 }
 
+class BinTree<T> {
+    _size: number;
+    _root: Node;
+    
+    updateHeight(x: Node) {}
+
+    size(): number {return this._size}
+    empty(): boolean {return !this._root}
+    root(): Node {return this._root}
+
+    traverse(){}
+
+
+}
