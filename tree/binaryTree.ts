9e4a4df954638dcdac3b0463cbe55e07a0e5bded
riverxs <sxj623@gmail.com>
2018-01-08 22:33:24 +0800

    modify binaryTree.ts

--- tree/binaryTree.ts
+++ tree/binaryTree.ts
@@ -1,77 +1,40 @@
-// 二叉树抽象数据类型
-
-// interface BinNode<T> {
-//     parent: BinNode<T>;
-//     left: BinNode<T>;
-//     right: BinNode<T>;
-
-//     data: T;
-//     height: number;
-    
-//     size(): number; // 子树规模
-
-//     insertAsLeftChild(left: T): BinNode<T>;
-//     insertAsRightChild(rigth: T): BinNode<T>;
-
-//     succ(): BinNode<T>;
-
-//     traverseLevel<U>(child: U): void; // 子树层次遍历
-//     traversePre<U>(child: U): void; // 子树先序遍历
-//     traverseIn<U>(child: U): void; // 子树中序遍历
-//     traversePost<U>(child: U): void; // 子树后序遍历
-// }
-
-
-class Node<T> {
-    // parent: BinNode<T>;
-    // data: 
-    left: Node;
-    right: Node;
-    height: number;
-    constructor(public data: T, public parent: Node) {
-        this.parent = parent;
-        this.data = data;
-        this.left = null;
-        this.right = null;
-        this.height = 0
-    }
-
-    insertAsLeftChild(e: T): void {
-        this.left = new Node(e, this)
-    }
-
-    insertAsRightChild(e: T){
-        this.right = new Node(e, this)
-    }
-
-    // 后代总数
-    size(): number {
-        var s: number = 1
-        if(this.left) s += this.left.size()
-        if(this.right) s += this.right.size()
-
-        return s
-    }
-
-    succ() {}
-
-    traverseLevel(){}
-    traversePre(){}
-    traverseIn(){}
-    traversePost(){}
+class TreeNode <T> {
+  left : TreeNode<T>;
+  right : TreeNode<T>;
+  constructor(public data : T, public parent : TreeNode<T>) {
+    this.data = data;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-class BinTree<T> {
-    _size: number;
-    _root: Node;
-    
-    updateHeight(x: Node) {}
+class BST<T> {
+  root : TreeNode<T>
+  _size : number
+  constructor() {
+    this.root = null
+    this._size = 0
+  }
 
-    size(): number {return this._size}
-    empty(): boolean {return !this._root}
-    root(): Node {return this._root}
+  get size() : number {return this._size}
 
-    traverse(){}
+  insert(data: T, parent: TreeNode<T>) {
+    if (!parent) {
+      if (!this.root) {
+        this.root = new TreeNode(data, null)
+        this._size++
+        return
+    }
 
+    const child = 
+    }
+  }
 
+  succ() {}
+
+  traverseLevel() {}
+  traversePre() {}
+  traverseIn() {}
+  traversePost() {}
 }
